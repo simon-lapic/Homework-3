ed72d3a873a354a8e028e1105d6a7cb4db5c136e
Simon <simonlapic@gmail.com>
2022-02-23 14:20:40 -0500

    Added a split functions and some initial implementations so that it builds

--- NASALaunchAnalysis.cpp
+++ NASALaunchAnalysis.cpp
@@ -8,14 +8,36 @@ using namespace std;
 #include "TimeCode.h"
 
 /**
+ * @brief Splits a given string along a given delimiter
+ * 
+ * @param str string, the string to split
+ * @param delim char, the delimiter to split along
+ * @return vector<string>, a list of the substrings between each delimiter
+ */
+vector<string> split(string str, char delim) {
+    vector<string> substrs;
+    int curr_start = 0;
+    for(int i = 0; i<str.size(); i++) {
+        if(str.at(i) == delim) {
+            substrs.push_back(str.substr(curr_start, i-curr_start));
+            curr_start = i+1;
+        }
+    }
+    return substrs;
+}
+
+/**
  * @brief Given a string, extracts a valid TimeCode, assuming that the seconds on the time code is 0
  * 
  * @param str The string to extract a TimeCode object from
  * @return TimeCode 
  */
 TimeCode extract_time_code(string str) {
-    str.substr(str.find_first_of(':')-2, 2);
-    return TimeCode();
+    string listed_tc = str.substr(str.find_first_of(':')-2, 4);
+    unsigned int hrs;
+    unsigned int mins;
+
+    return TimeCode(1, 1, 0);
 }
 
 /**
@@ -29,12 +51,35 @@ vector<TimeCode> get_time_codes(string fileName) {
     vector<TimeCode> time_codes;
 
     string line;
+    getline(file, line);
+    int count = 0;
     while(getline(file, line)) {
-        time_codes.push_back(extract_time_code(line));
+        // Get the TimeCode from the 4th column of the csv line
+        vector<string> columns = split(line, '\"');
+        cout << "Line " << count << ": " << columns.at(1) << endl;
+        count++;
+        time_codes.push_back(extract_time_code(columns.at(1)));
     }
-    return vector<TimeCode>();
+    return time_codes;
+}
+
+/**
+ * @brief Averages a vector of TimeCodes
+ * 
+ * @param time_codes vector<TimeCode>, the list of TimeCodes to average
+ * @return TimeCode 
+ */
+TimeCode avg_time_codes(vector<TimeCode> time_codes) {
+    double counter = 0;
+    TimeCode sum;
+    for(TimeCode tc : time_codes) {
+        counter++;
+        sum = sum + tc;
+    }
+
+    return sum / counter;
 }
 
 int main() {
-
+    cout << "Average Launch Time: " << avg_time_codes(get_time_codes("Space_Corrected.csv")).ToString() << endl;
 }
